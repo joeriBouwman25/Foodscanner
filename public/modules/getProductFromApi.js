4acdf614149d5feb64c7c6f7b22727a02ef64994
joeribouwman25 <joeri-bouwman@live.nl>
2022-03-04 17:28:55 +0100

    issues

--- public/modules/getProductFromApi.js
+++ public/modules/getProductFromApi.js
@@ -1,17 +1,15 @@
 import { defineProductData } from "./defineProductData.js"
 
-async function getProductFromApi(barcode){
+export async function getProductFromApi(barcode){
     const url = 'https://world.openfoodfacts.org/api/v0/product/'
     const data = await fetch(url + barcode + 'json')
     const res = await data.json()
 
               if(res.status == 1){ 
                 defineProductData( barcode, res)
-                console.log(res)
           } else {
             window.location.hash = "#error"
             console.log(res.status_verbose)
           }
 }
 
-export{ getProductFromApi }
